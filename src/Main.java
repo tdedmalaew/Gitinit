fef8d0b65fa2b60e4f3451e559cc33aaaf730814
firsticz <thanawat6368@gmail.com>
2018-10-18 11:41:10 +0700

    add Main class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name = in.next();
+		Hello hello = new Hello();
+		hello.setName(name);
+		System.out.println("Hello "+.getName);
+
+	}
+
+}
